33d11f90abf8873e536ebf684ed9f4fd862266ca
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-29 13:33:45 +0200

    Add Lunte, publish workflow, and use `bare-build` v1

--- runtime.js
+++ runtime.js
@@ -1,3 +1,7 @@
+const { type } = require('bare-build/constants')
+
+const { EXECUTABLE } = type
+
 require.asset = require('require-asset')
 
 const prebuilds = {}
@@ -5,9 +9,17 @@ const prebuilds = {}
 exports.prebuilds = prebuilds
 
 prebuilds['linux-x64'] = () => {
-  return require.asset('./prebuilds/linux-x64/bare', __filename)
+  return {
+    type: EXECUTABLE,
+    path: require.asset('./prebuilds/linux-x64/bare', __filename),
+    dependencies: []
+  }
 }
 
 prebuilds['linux-arm64'] = () => {
-  return require.asset('./prebuilds/linux-arm64/bare', __filename)
+  return {
+    type: EXECUTABLE,
+    path: require.asset('./prebuilds/linux-arm64/bare', __filename),
+    dependencies: []
+  }
 }
